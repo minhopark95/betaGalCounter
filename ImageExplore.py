71c95622279c4c912495b6eae11e0d4d4a90ed83
Ben Wesley <benkumwes@gmail.com>
2020-05-10 15:12:51 -0400

    Cleaning up code and finalizing variables to deal with different noise and file types

--- ImageExplore.py
+++ ImageExplore.py
@@ -6,68 +6,59 @@ import statistics as stats
 from matplotlib import pyplot as plt
 from matplotlib import mlab
 
-SIGMA_MULTIPLIER = 4
+BLUE_SIGMA_MULTIPLIER = 4
+CELL_SIGMA_MULTILIER = 4
 G_RES_MULTIPLER = 0.2
-SIG_THRESH_MULT = 0.3
+CONTRAST_MULTIPLIER = 0.75
+CLEANUP_LOOPS = 2
 
 
-def gradient_correction(backImg, dataImg):
+def increaseContrast(img):
+    # get stats real quick
+    mu, sigma = norm.fit(img.ravel())
+
     # Shrink and apply median blur and expand to make a gradient image (g_img)
-    g_img = cv2.resize(backImg, None, fx=G_RES_MULTIPLER, fy=G_RES_MULTIPLER, interpolation=cv2.INTER_NEAREST)
+    g_img = cv2.resize(img, None, fx=G_RES_MULTIPLER, fy=G_RES_MULTIPLER, interpolation=cv2.INTER_NEAREST)
     g_img = cv2.medianBlur(g_img, 7)
-    g_img = cv2.subtract(g_img, 4*sigma)
-    g_img = cv2.resize(src = g_img, dsize = (img.shape[1], img.shape[0]), interpolation=cv2.INTER_AREA)
+    g_img = cv2.subtract(g_img, CELL_SIGMA_MULTILIER * sigma)
+    g_img = cv2.resize(src=g_img, dsize=(img.shape[1], img.shape[0]), interpolation=cv2.INTER_AREA)
 
-    #subtract the gradient image from the data channel
-    img = cv2.subtract(dataImg, g_img)
+    # subtract the gradient image from the data channel
+    img = cv2.subtract(img, g_img)
+    return img
 
-    #get centering statistics to identify the cells
+
+def identifyCells(img):
+    # get centering statistics to identify the cells
     mode = stats.mode(img.ravel())
     mu, sigma = norm.fit(img.ravel())
 
-    #threshold image
-    threshLevel = mode - 0.75*sigma
+    # threshold image
+    threshLevel = mode - CONTRAST_MULTIPLIER * sigma
     ___, thresh_img = cv2.threshold(img, threshLevel, 255, cv2.THRESH_BINARY)
 
-    noiseKern = np.ones((2,2), np.uint8)
-    eroded = cv2.dilate(thresh_img, noiseKern)
-
-    cellKern = np.ones((3,3), np.uint8)
-    dilated = cv2.erode(eroded, noiseKern)
+    # do an erode followed by dialation x2
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    closed = thresh_img
+    for i in range(CLEANUP_LOOPS):
+        closed = cv2.morphologyEx(closed, cv2.MORPH_CLOSE, kernel)
 
-    # First dialate, then erode (morph close), then dialate a whole bunch
-    closed = cv2.morphologyEx(thresh_img, cv2.MORPH_CLOSE, kernel)
+    # Invert the image and find contours
+    closed = cv2.bitwise_not(closed)
+    ____, contours, _____ = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+    # Go smoothen cell exteriors with convex hull and drop any small cells
+    hull = []
+    for i in range(len(contours)):
+        size = cv2.contourArea(contours[i])
+        if size > 10:
+            hull.append(cv2.convexHull(contours[i], False))
 
-
+    # draw the convex hulls
+    cv2.drawContours(closed, hull, -1, 123, 2)
 
     cv2.imshow('thresholded image', closed)
-    # n, bins, patches = plt.hist(img.ravel(), 256, [0, 255], normed=True)
-    # normalCurve = norm.pdf(bins, mode, sigma)
-    # plt.plot(bins, normalCurve, 'r--', linewidth=2)
-    # plt.show()
-    # make the correction map, a map of each pixel's deviation from the average
-#    for col in range(height):
-#        c_map.append([])
-#        for row in range(width):
-#            c_map[col].append(c_value - g_img[col][row])
-
-#    big_height, big_width = img.shape[:2]
-
-    # apply the correction map to the corresponding pixel in the full sized image
-#    for col in range(big_height):
-#        for row in range(big_width):
-#            c_col = int(col * g_res)
-#            c_row = int(row * g_res)
-#            sig_thresh = g_img[c_col][c_row] * SIG_THRESH_MULT
-#            if img[col][row] > sig_thresh:  # reverse the < to undo comments
-#                img[col][row] = 0
-#            else:  # Set colonies to 0 for circle detect
-#                img[col][row] = 255
-
-#    del g_img
-#    del c_map
+    cv2.imshow('original image', img)
 
     return img
 
@@ -75,9 +66,9 @@ def gradient_correction(backImg, dataImg):
 def cellFinder(file: chr):
     img = cv2.imread('./Test Images/' + file)
     r, g, b = cv2.split(img)
-    print(g.shape)
-    g = gradient_correction(g)
-    # cv2.imshow('gradient corrected image', g)
+
+    r = increaseContrast(img=r)
+    r = identifyCells(img=r)
     cv2.waitKey()
 
 
@@ -88,7 +79,7 @@ def bGalFinder(file: chr):
     bDiff = cv2.subtract(r, b)
 
     mu, sigma = norm.fit(bDiff.ravel())
-    thresh_val = mu + SIGMA_MULTIPLIER * sigma
+    thresh_val = mu + BLUE_SIGMA_MULTIPLIER * sigma
     __, bDiffThresh = cv2.threshold(bDiff, thresh_val, 123, cv2.THRESH_BINARY)
 
     # Plot out with matplotlib to get a pixel location
